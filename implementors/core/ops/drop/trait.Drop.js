af6aa305d16575a32e2e80196c38958f1b23c993
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2018-09-16 07:22:31 +0200

    updated documentation

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["ubend"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ubend/struct.Chain.html\" title=\"struct ubend::Chain\">Chain</a>",synthetic:false,types:["ubend::Chain"]},];
+implementors["ubend"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ubend/struct.Child.html\" title=\"struct ubend::Child\">Child</a>",synthetic:false,types:["ubend::Child"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
