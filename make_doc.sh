0cee104558232d2981b110bd17ca8f6290dae93b
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2018-09-16 07:24:37 +0200

    improved make_doc.sh

--- make_doc.sh
+++ make_doc.sh
@@ -10,5 +10,6 @@ cargo doc --no-deps
 git checkout gh-pages
 rm -rv *.woff *.svg *.js *.txt *.css src/ubend implementors ubend
 cp -r target/doc/* .
+git add ubend src/ubend implementors
 git commit -a -m "updated documentation"
 git push
